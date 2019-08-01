b522acf78fd3d4bb672ff34e9526111d7863f7e2
Michael Mooney <51172274+MichaelCMooney@users.noreply.github.com>
2019-08-01 15:51:50 -0400

    MAC Changer

---
+++ macchanger.py
@@ -0,0 +1,6 @@
+import subprocess
+
+subprocess.call("ifconfig wlan0 down", shell=True)
+subprocess.call("ifconfig wlan0 hw ether 00:11:22:33:44:55", shell=True)
+subprocess.call("ifconfig wlan0 up", shell=True)
+
