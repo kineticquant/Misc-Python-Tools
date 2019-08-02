20d2763a5fb244659768a2f37e19ad0fc52289f9
Michael Mooney <51172274+MichaelCMooney@users.noreply.github.com>
2019-08-02 09:44:44 -0400

    Update macchanger.py

--- macchanger.py
+++ macchanger.py
@@ -1,6 +1,10 @@
 import subprocess
 
-subprocess.call("ifconfig wlan0 down", shell=True)
-subprocess.call("ifconfig wlan0 hw ether 00:11:22:33:44:55", shell=True)
-subprocess.call("ifconfig wlan0 up", shell=True)
+interface = "wlan0"
+new_mac = "00:11:22:33:44:77"
 
+print("[+] Changing MAC address for " + interface + " to " + new_mac)
+
+subprocess.call("ifconfig wlan0 down", shell=True)
+subprocess.call("ifconfig wlan0 hw ether 00:11:22:33:44:77", shell=True)
+subprocess.call("ifconfig wlan0 up", shell=True)
