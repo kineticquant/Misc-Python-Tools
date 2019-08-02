1f5729ef213a3bbc32d4f10e71c66fe11839b9cf
Michael Mooney <51172274+MichaelCMooney@users.noreply.github.com>
2019-08-02 10:16:30 -0400

    Adjusted variable placement

--- macchanger.py
+++ macchanger.py
@@ -1,3 +1,6 @@
+#For use on Linux only
+#Python dependency
+
 import subprocess
 
 interface = "wlan0"
@@ -5,6 +8,6 @@ new_mac = "00:11:22:33:44:77"
 
 print("[+] Changing MAC address for " + interface + " to " + new_mac)
 
-subprocess.call("ifconfig wlan0 down", shell=True)
-subprocess.call("ifconfig wlan0 hw ether 00:11:22:33:44:77", shell=True)
-subprocess.call("ifconfig wlan0 up", shell=True)
+subprocess.call("ifconfig " + interface + " down", shell=True)
+subprocess.call("ifconfig " + interface + " hw ether " + new_mac, shell=True)
+subprocess.call("ifconfig " + interface + " up", shell=True)
